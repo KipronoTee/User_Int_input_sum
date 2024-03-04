1b628b96f6cf951e162c7483a980ee62749cc726
KipronoTee <kipronotc@gmail.com>
2024-03-04 20:26:41 +0300

    first commit

---
+++ Fav_books.py
@@ -0,0 +1,5 @@
+#  list of favorite books
+Favorite_books = ("Think_and_grow_rich","Fear Trump in the White House", "Becoming", "The Republic", "Democracy in America")
+
+for Think_and_grow_rich in Favorite_books:
+    print(Think_and_grow_rich)

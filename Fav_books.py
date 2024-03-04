31d49a5c8c98286ac0297b5c4ff3002aa88b8dee
KipronoTee <kipronotc@gmail.com>
2024-03-04 21:11:17 +0300

    first commit

--- Fav_books.py
+++ Fav_books.py
@@ -1,5 +1,6 @@
 #  list of favorite books
 Favorite_books = ("Think_and_grow_rich","Fear Trump in the White House", "Becoming", "The Republic", "Democracy in America")
 
+# access list of books in the tuple
 for Think_and_grow_rich in Favorite_books:
     print(Think_and_grow_rich)
---
+++ Governor1_dict.py
@@ -0,0 +1,12 @@
+# ask use for name
+name = input("what is your name?  ")
+
+#ask user for age
+age = input("what is your age? ")
+
+# ask user  for favorite color
+Fav_color = input("what is your favorite? ")
+
+Governor1 = {"name":  "Governor1", "age":  42, "Fav_color": "Blue"}
+
+print(Governor1)
